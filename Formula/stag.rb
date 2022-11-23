d91be639d6122df238b54d1eaa59d375264646c4
Zyler <508282+zylerdj@users.noreply.github.com>
2022-11-23 04:40:08 +0100

    Correct S3 link

--- Formula/stag.rb
+++ Formula/stag.rb
@@ -4,7 +4,7 @@
 class Stag < Formula
   desc "Bitcoin Wallet for Machines & Humans Alike"
   homepage "https://stagwallet.io"
-  url "s3://stagwallet/0.1.2/stag-v0.1.2-8a09f90-x64.pkg"
+  url "https://stagwallet.s3.amazonaws.com/0.1.2/stag-v0.1.2-8a09f90-x64.pkg"
   sha256 "90cb84b252dafb2dac56260350c9c997758d7a1ca999b749e60b049a99aedf4d"
   version "0.1.2"
 
